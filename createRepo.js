b38a3575b1ecccedf27e5bcc42da3a4ad9a81c21
DaBossRageGamrMan2173 <ereyes2155@gmail.com>
2024-07-17 15:47:11 -0600

    Update createRepo.js

--- createRepo.js
+++ createRepo.js
@@ -1,1 +1,29 @@
 touch createRepo.js
+
+const axios = require('axios');
+
+const createRepo = async (repoName) => {
+  const token = 'YOUR_GITHUB_ACCESS_TOKEN';
+  const url = 'https://api.github.com/user/repos';
+
+  const headers = {
+    'Authorization': `token ${token}`,
+    'Accept': 'application/vnd.github.v3+json'
+  };
+
+  const data = {
+    name: repoName,
+    private: false,
+  };
+
+  try {
+    const response = await axios.post(url, data, { headers });
+    console.log('Repository created:', response.data.html_url);
+  } catch (error) {
+    console.error('Error creating repository:', error.response.data);
+  }
+};
+
+const repos = ['card-game-frontend', 'card-game-backend'];
+
+repos.forEach(repo => createRepo(repo));
